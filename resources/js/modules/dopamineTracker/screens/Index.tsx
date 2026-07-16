3b74348f23ec546ade3edbf35e3902acad6f50cd
Traore Allassane <traoreallassane2255@gmail.com>
2026-07-16 08:36:58 +0000

    fix(Module Dopamine Tracker) : Nettoyage du timeline lorsqu'on change de date

--- resources/js/modules/dopamineTracker/screens/Index.tsx
+++ resources/js/modules/dopamineTracker/screens/Index.tsx
@@ -50,6 +50,9 @@ export default function Index() {
         useStimulus();
 
     const datePrecedente = () => {
+        // Nettoyage du timeline
+        setStimulus([]);
+
         setSelectedDate((prev) => {
             const date = new Date(prev);
             date.setDate(date.getDate() - 1);
@@ -58,6 +61,9 @@ export default function Index() {
     };
 
     const dateSuivante = () => {
+        // Nettoyage du timeline
+        setStimulus([]);
+
         setSelectedDate((prev) => {
             const date = new Date(prev);
             date.setDate(date.getDate() + 1);
